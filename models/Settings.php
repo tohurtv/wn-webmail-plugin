bd095a0d5ac84d0e31b6959a05647e5a31a87c97
tohurtv <joshwebb84@outlook.com>
2025-06-23 09:15:37 -0600

    settings fix

--- models/Settings.php
+++ models/Settings.php
@@ -7,5 +7,5 @@ class Settings extends Model
     public $implement = ['System.Behaviors.SettingsModel'];
 
     public $settingsCode = 'tohur_webmail_settings';
-    public $settingsFields = 'config/settings.yaml';
+    public $settingsFields = 'fields.yaml';
 }
